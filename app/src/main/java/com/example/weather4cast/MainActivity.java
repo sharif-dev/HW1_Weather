f47b067e583bd96f56b7ac4867fc6986d789e08d
Ali <aj.ali.javan@gmail.com>
2020-04-08 22:49:26 +0430

    activity_search xml added

--- app/src/main/java/com/example/weather4cast/MainActivity.java
+++ app/src/main/java/com/example/weather4cast/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.weather4cast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search);
+
+        ListView regionsList = (ListView) findViewById(R.id.regionsList);
     }
 }
