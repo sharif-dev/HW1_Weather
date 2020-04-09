b8fe34ca45f5a6eedd8b2cf57fafd7cf518009cc
Ali <aj.ali.javan@gmail.com>
2020-04-09 20:39:04 +0430

    thread run changed to start

--- app/src/main/java/com/example/weather4cast/MainActivity.java
+++ app/src/main/java/com/example/weather4cast/MainActivity.java
@@ -113,6 +113,6 @@ public class MainActivity extends AppCompatActivity {
                 queue.add(stringRequest);
             }
         });
-        searchThread.run();
+        searchThread.start();
     }
 }
